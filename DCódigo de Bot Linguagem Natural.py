9ad60ca1f0fe568d341b1c0b9bdef232dae9f0e0
douglasmd3 <74698250+douglasmd3@users.noreply.github.com>
2020-11-22 20:14:43 -0300

    Update DCódigo de Bot Linguagem Natural.py

--- DCódigo de Bot Linguagem Natural.py
+++ DCódigo de Bot Linguagem Natural.py
@@ -1,3 +1,41 @@
 #arquivo de código de bot para telegram com liguangem natural, ainda em andamento, objetivando juntar ao código de linguagem programada em arquivo igual ou diferente 
 #e importar, a definir como será a melhor programação. dúvida e problema, ambos os códigos dever ser executados e, interagindo com o usuário definir os critérios e
 #prioridades para retornar do código os pontos de linguagem natural ou programada... 
+
+# -*- coding: utf-8 -*-
+# [sudo] pip3 install telepot chatterbot
+# [sudo] pip3 install -U spacy
+# [sudo] python3 -m spacy download en 
+# [sudo] python3 -m spacy download pt
+
+import telepot
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+
+bot = ChatBot("IFRN/SGA")
+trainer = ListTrainer(bot)
+
+Arquivo = open("Treinamento.txt","r").readlines()
+trainer.train(Arquivo)
+
+
+token = telepot.Bot("1461971673:AAFCplUD52RZEU2tM47b0Mr1Xf8AmKxjZts")
+
+print ("\nExecutando o Código...Good Luck\n")
+
+
+def receber_mensagem(self):
+    text = self['text']
+    _id = self['from']['id']
+    nome = self['from']['first_name']
+    A = token.sendMessage(_id, str(bot.get_response(text)))
+    
+    print('id: {} \n{} > {} \nRespostaBot > {}'.format(_id,nome,text,A['text']))
+    print("\n", "-" * 50)
+
+token.message_loop(receber_mensagem)
+
+
+while True:
+    pass
